82faa0f63b72bf3ab7c777dc5868f0033ef64c09
Olha Vernyhora <47662865+olhavernyhora@users.noreply.github.com>
2019-05-22 15:37:48 +0300

    Collections Framework

---
+++ Java_tasks/Collection/task1/Main.java
@@ -0,0 +1,19 @@
+package task1;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int size = 8;
+        int minElement = -3;
+        int maxElement = 20;
+
+        GenerateCollection instances = new GenerateCollection(size, minElement, maxElement);
+        System.out.println(instances);
+
+
+
+
+    }
+}
